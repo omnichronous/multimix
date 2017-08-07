3c7f13c86860d63fc34bfebead6b03974e2e0012
Mitko Georgiev <omnichronous@gmail.com>
2017-08-07 11:49:50 +0300

    specified package versions, updated for Laravel Mix 1.4.2

---
+++ resources/assets/admin/js/admin.js
@@ -0,0 +1,2 @@
+// search for this in /public_html/assets/admin/js/global.js to verify successful compilation
+console.log('avocado');
---
+++ resources/assets/website/js/website.js
@@ -0,0 +1,2 @@
+// search for this in /public_html/assets/website/js/global.js to verify successful compilation
+console.log('watermelon');
